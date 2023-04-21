03d8c162a9bcd9b31558294a8f1f80788db7b3ac
hermes7144 <kichancho@gmail.com>
2023-04-21 22:35:26 +0900

    feat:markdown 구현 완료

--- src/app/components/MarkdownViewer.tsx
+++ src/app/components/MarkdownViewer.tsx
@@ -1,12 +1,15 @@
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+'use client';
+
+import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from 'next/image';
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
     <ReactMarkdown
-      className='prose'
+      className='prose max-w-none'
       remarkPlugins={[remarkGfm]}
       components={{
         code({ node, inline, className, children, ...props }) {
@@ -21,6 +24,7 @@ export default function MarkdownViewer({ content }: { content: string }) {
             </code>
           );
         },
+        img: (image) => <Image className='w-full max-h-60 object-cover' src={image.src || ''} alt={image.alt || ''} width={500} height={350} />,
       }}>
       {content}
     </ReactMarkdown>
